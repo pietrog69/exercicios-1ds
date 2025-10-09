b9fa2cf733556842553fab77d0713ded7e237b48
Pietro Gabriel Ferreira Viana <pietro.viana3@aluno.ce.gov.br>
2025-10-09 17:42:52 +0000

    alterado

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,10 @@
-// CRIE UMA LÓGICA AQUI
+let resultado = 1
+
+while (resultado < 10) {
+    resultado++
+    console.log(`${resultado}`)
+}
+
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
