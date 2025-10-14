111bb8ca034fc301138e71c2db0b701490ef0717
Pietro Gabriel Ferreira Viana <pietro.viana3@aluno.ce.gov.br>
2025-10-14 14:47:18 +0000

    concluido

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,10 @@
-// CRIE UMA LÓGICA AQUI
+let resultado = 0
+
+for (let i = 1; i <= 10; i++) {
+    resultado = resultado + i
+    console.log(`${resultado}}`)
+}
+
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
